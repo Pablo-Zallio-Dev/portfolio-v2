1bf160be8dd1b0afc24df2be6c6acb3a708e052f
Pablo Zallio <pablozalliodev@gmail.com>
2026-07-20 13:02:04 +0200

    fix: comprobe email

--- app/components/ui/Form.tsx
+++ app/components/ui/Form.tsx
@@ -19,11 +19,14 @@ export default function Form() {
 
 
       const onSubmit = async (data: FormValues) => {
+             console.log("onSubmit ejecutado");
+  console.log(data);
             try {
                   setSend(true);
 
                   const formData = new FormData();
 
+                  console.log(process.env.NEXT_PUBLIC_WEB3FORMS_KEY);
                   formData.append("access_key", process.env.NEXT_PUBLIC_WEB3FORMS_KEY!);
                   formData.append("name", data.name);
                   formData.append("email", data.email);
